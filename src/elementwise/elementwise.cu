e189f9a2723a6d6fe66f1c2863c064cd1789c39d
criscuolosubidu <criscuolosubidu@gmail.com>
2025-07-18 17:17:45 +0800

    [add] 开刷leetcuda了，一边看代码，一边看programming guide来补基础

---
+++ src/elementwise/elementwise.cu
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <cuda_bf16.h>
+#include <cuda_fp16.h>
+#include <cuda_fp8.h>
+#include <cuda_runtime.h>
+#include <float.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+
+
+__global__ void elementwise_add_f32_kernel(float *a, float *b, float *c, int N) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < N) c[idx] = a[idx] + b[idx];
+}
+
